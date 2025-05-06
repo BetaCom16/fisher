7318e4481e317b27ce3f6f2807895ec93940843b
erik <betacom16@outlook.de>
2025-05-06 19:59:35 +0200

    Repository hinzugefügt

---
+++ src/main/java/sheasepherd/fisher/repositorys/GeisternetzRepository.java
@@ -0,0 +1,7 @@
+package sheasepherd.fisher.repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sheasepherd.fisher.entitys.Geisternetz;
+
+public interface GeisternetzRepository extends JpaRepository<Geisternetz, Long> {
+}
