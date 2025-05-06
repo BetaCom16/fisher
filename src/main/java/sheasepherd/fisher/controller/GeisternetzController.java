b637fb6aae091c77a3a98d9cac33a3f845b7592e
erik <betacom16@outlook.de>
2025-05-06 20:00:17 +0200

    Geisternetze werden zukünftigt auch in der Datenbank gespeichert

--- src/main/java/sheasepherd/fisher/controller/GeisternetzController.java
+++ src/main/java/sheasepherd/fisher/controller/GeisternetzController.java
@@ -1,13 +1,19 @@
 package sheasepherd.fisher.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sheasepherd.fisher.entitys.Geisternetz;
+import sheasepherd.fisher.repositorys.GeisternetzRepository;
 
 @Controller
 public class GeisternetzController {
+
+    @Autowired
+    private GeisternetzRepository geisternetzRepository;
 
     @GetMapping("/melden")
     public String zeigeFormular(){
@@ -26,6 +32,12 @@ public class GeisternetzController {
             model.addAttribute("latitude", latitude);
             model.addAttribute("longtitude", longtitude);
             model.addAttribute("groesse", groesse);
+
+            Geisternetz geisternetz = new Geisternetz();
+            geisternetz.setLatitude(latitude);
+            geisternetz.setLongitude(longtitude);
+            geisternetz.setSize(groesse);
+            geisternetzRepository.save(geisternetz);
 
             return "bestaetigung";
     }
