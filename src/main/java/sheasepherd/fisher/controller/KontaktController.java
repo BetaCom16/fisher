b88f621c97a955813d0954bea493626ecdb2d5b4
Erik <betacom16@outlook.de>
2025-05-07 17:57:39 +0200

    Controller und Template für die Seite Kontakt hinzugefügt

---
+++ src/main/java/sheasepherd/fisher/controller/KontaktController.java
@@ -0,0 +1,14 @@
+package sheasepherd.fisher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class KontaktController {
+
+    @GetMapping("/kontakt")
+    public String kontakt(){
+        return "kontakt";
+    }
+
+}
