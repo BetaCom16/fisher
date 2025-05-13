5d78c4e5236da3c2af8776215b27d847721294da
Erik <betacom16@outlook.de>
2025-05-13 18:46:45 +0200

    Testseite hinzugefügt

---
+++ src/main/java/sheasepherd/fisher/controller/TestController.java
@@ -0,0 +1,14 @@
+package sheasepherd.fisher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String zeigeTestseite(){
+        return "test";
+    }
+
+}
