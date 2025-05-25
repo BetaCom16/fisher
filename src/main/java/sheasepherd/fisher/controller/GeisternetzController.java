84d3f226f2f763b58620e35ffff7d60e214a8d78
Erik <betacom16@outlook.de>
2025-05-25 20:44:52 +0200

    ID Verknüpfung zwischen den Datenbanken Mitglied/Geisternetz hergestellt und Anonym-Status überarbeitet

--- src/main/java/sheasepherd/fisher/controller/GeisternetzController.java
+++ src/main/java/sheasepherd/fisher/controller/GeisternetzController.java
@@ -1,13 +1,17 @@
 package sheasepherd.fisher.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import sheasepherd.fisher.entitys.Geisternetz;
+import sheasepherd.fisher.entitys.Mitglied;
 import sheasepherd.fisher.repositorys.GeisternetzRepository;
+import sheasepherd.fisher.services.MitgliedService;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -20,6 +24,9 @@ public class GeisternetzController {
 
     @Autowired
     private GeisternetzRepository geisternetzRepository;
+
+    @Autowired
+    private MitgliedService MitgliedService;
 
     @GetMapping("/melden")
     public String zeigeFormular(){
@@ -36,6 +43,7 @@ public class GeisternetzController {
         @RequestParam("latitude") double latitude,
         @RequestParam("longitude") double longitude,
         @RequestParam("groesse") String groesse,
+        @RequestParam(value = "anonym", defaultValue = "false", required = false) boolean anonym,
         Model model){
 
             Geisternetz geisternetz = new Geisternetz();
@@ -44,7 +52,17 @@ public class GeisternetzController {
             geisternetz.setLongitude(longitude);
             geisternetz.setSize(groesse);
             geisternetz.setStatus("Gemeldet");
+            geisternetz.setAnonym(anonym);
             geisternetz.setDate(dateFormatter.format(currentTime));
+
+            if(!anonym){
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                String email = authentication.getName();
+                Mitglied mitglied = MitgliedService.findByEmail(email);
+                if(mitglied != null){
+                    geisternetz.setMeldendePerson(mitglied);
+                }
+            }
 
             geisternetzRepository.save(geisternetz);
 
@@ -53,6 +71,7 @@ public class GeisternetzController {
             model.addAttribute("latitude", latitude);
             model.addAttribute("longitude", longitude);
             model.addAttribute("groesse", groesse);
+            model.addAttribute("anonym", anonym);
             model.addAttribute("date", geisternetz.getDate());
 
             return "bestaetigung";
